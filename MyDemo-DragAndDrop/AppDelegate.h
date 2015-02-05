971ecff01c2673a2b614a789278de86c803a6f61
OliHire-HellowJingQiu <hellowjingqiu@OliHire-HellowJQ.local>
2015-02-05 11:32:20 +0800

    first

---
+++ MyDemo-DragAndDrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDemo-DragAndDrop
+//
+//  Created by OliHire-HellowJingQiu on 15/2/5.
+//  Copyright (c) 2015年 CongYi-HellowJingQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo-DragAndDrop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyDemo-DragAndDrop
+//
+//  Created by OliHire-HellowJingQiu on 15/2/5.
+//  Copyright (c) 2015年 CongYi-HellowJingQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
